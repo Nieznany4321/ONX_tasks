87643ee70433a8f959d6e618c361764c97619b97
Dawid Wilgucki <dawid123wilgucki@wp.pl>
2023-03-25 22:12:21 +0100

    Notification

--- laravel_app/app/Notifications/CarsNotify.php
+++ laravel_app/app/Notifications/CarsNotify.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class carsNotify extends Notification
+class CarsNotify extends Notification
 {
     use Queueable;
     private $carsAssigned;
